b758dc3d3e05969a007422c31cd8ff6d71fa2c54
Peltola Leevi <leevi.peltola@hiq.fi>
2019-11-04 15:50:23 +0200

    fcom221

---
+++ Frends.Community.PgpVerifyClearTextSignature/PgpVerifyClearTextSignatureTask.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Org.BouncyCastle.Bcpg;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using System.IO;
+using System.Text;
+
+#pragma warning disable 1591
+
+namespace FRENDS.Community.PgpVerifyClearTextSignature
+{
+
+    public class PgpVerifyClearTextSignatureTask
+    {
+        /// <summary>
+        /// Verifies clear text PGP signature. See documentation at https://github.com/CommunityHiQ/Frends.Community.PgpVerifyClearTextSignature Returns: Object {string FilePath, Boolean Verified}
+        /// </summary>
+        public static Result PGPVerifyClearTextSignFile(Input input)
+        {
+            Stream inStr = File.OpenRead(input.InputFile);
+            ArmoredInputStream aIn = new ArmoredInputStream(inStr);
+            Stream outStr = File.Create(input.OutputFile);
+
+            //
+            // write out signed section using the local line separator.
+            // note: trailing white space needs to be removed from the end of
+            // each line RFC 4880 Section 7.1
+            //
+            MemoryStream lineOut = new MemoryStream();
+            int lookAhead = ReadInputLine(lineOut, aIn);
+            byte[] lineSep = Encoding.ASCII.GetBytes(Environment.NewLine); ;
+
+
+            if (lookAhead != -1 && aIn.IsClearText())
+            {
+                byte[] line = lineOut.ToArray();
+                outStr.Write(line, 0, GetLengthWithoutSeparatorOrTrailingWhitespace(line));
+                outStr.Write(lineSep, 0, lineSep.Length);
+
+                while (lookAhead != -1 && aIn.IsClearText())
+                {
+                    lookAhead = ReadInputLine(lineOut, lookAhead, aIn);
+
+                    line = lineOut.ToArray();
+                    outStr.Write(line, 0, GetLengthWithoutSeparatorOrTrailingWhitespace(line));
+                    outStr.Write(lineSep, 0, lineSep.Length);
+                }
+            }
+            else
+            {
+                // a single line file
+                if (lookAhead != -1)
+                {
+                    byte[] line = lineOut.ToArray();
+                    outStr.Write(line, 0, GetLengthWithoutSeparatorOrTrailingWhitespace(line));
+                    outStr.Write(lineSep, 0, lineSep.Length);
+                }
+            }
+            outStr.Close();
+
+            PgpPublicKeyRingBundle pgpRings = new PgpPublicKeyRingBundle(PgpUtilities.GetDecoderStream(File.OpenRead(input.PublicKeyFile)));
+
+            PgpObjectFactory pgpFact = new PgpObjectFactory(aIn);
+            PgpSignatureList p3 = (PgpSignatureList)pgpFact.NextPgpObject();
+            PgpSignature sig = p3[0];
+            inStr.Close();
+
+            sig.InitVerify(pgpRings.GetPublicKey(sig.KeyId));
+            // read the input, making sure we ignore the last newline.
+            Stream sigIn = File.OpenRead(input.OutputFile);
+            lookAhead = ReadInputLine(lineOut, sigIn);
+            ProcessLine(sig, lineOut.ToArray());
+            if (lookAhead != -1)
+            {
+                do
+                {
+                    lookAhead = ReadInputLine(lineOut, lookAhead, sigIn);
+
+                    sig.Update((byte)'\r');
+                    sig.Update((byte)'\n');
+
+                    ProcessLine(sig, lineOut.ToArray());
+                }
+                while (lookAhead != -1);
+            }
+
+            bool verified = sig.Verify();
+            sigIn.Close();
+            Result ret = new Result
+            {
+                FilePath = input.OutputFile,
+                Verified = verified
+            };
+
+            return ret;
+
+        }
+
+        private static int ReadInputLine(MemoryStream bOut, int lookAhead, Stream fIn)
+        {
+            bOut.SetLength(0);
+
+            int ch = lookAhead;
+
+            do
+            {
+                bOut.WriteByte((byte)ch);
+                if (ch == '\r' || ch == '\n')
+                {
+                    lookAhead = ReadPassedEol(bOut, ch, fIn);
+                    break;
+                }
+            }
+            while ((ch = fIn.ReadByte()) >= 0);
+
+            if (ch < 0)
+            {
+                lookAhead = -1;
+            }
+
+            return lookAhead;
+        }
+
+        private static int ReadInputLine(MemoryStream bOut, Stream fIn)
+        {
+            bOut.SetLength(0);
+
+            int lookAhead = -1;
+            int ch;
+
+            while ((ch = fIn.ReadByte()) >= 0)
+            {
+                bOut.WriteByte((byte)ch);
+                if (ch == '\r' || ch == '\n')
+                {
+                    lookAhead = ReadPassedEol(bOut, ch, fIn);
+                    break;
+                }
+            }
+
+            return lookAhead;
+        }
+
+        private static int ReadPassedEol(MemoryStream bOut, int lastCh, Stream fIn)
+        {
+            int lookAhead = fIn.ReadByte();
+
+            if (lastCh == '\r' && lookAhead == '\n')
+            {
+                bOut.WriteByte((byte)lookAhead);
+                lookAhead = fIn.ReadByte();
+            }
+
+            return lookAhead;
+        }
+
+        private static int GetLengthWithoutSeparatorOrTrailingWhitespace(byte[] line)
+        {
+            int end = line.Length - 1;
+
+            while (end >= 0 && IsWhiteSpace(line[end]))
+            {
+                end--;
+            }
+
+            return end + 1;
+        }
+        private static bool IsWhiteSpace(byte b)
+        {
+            return IsLineEnding(b) || b == '\t' || b == ' ';
+        }
+
+        private static bool IsLineEnding(byte b)
+        {
+            return b == '\r' || b == '\n';
+        }
+
+        private static void ProcessLine(PgpSignature sig, byte[] line)
+        {
+            // note: trailing white space needs to be removed from the end of
+            // each line for signature calculation RFC 4880 Section 7.1
+            int length = GetLengthWithoutWhiteSpace(line);
+            if (length > 0)
+            {
+                sig.Update(line, 0, length);
+            }
+        }
+
+        private static int GetLengthWithoutWhiteSpace(byte[] line)
+        {
+            int end = line.Length - 1;
+
+            while (end >= 0 && IsWhiteSpace(line[end]))
+            {
+                end--;
+            }
+
+            return end + 1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
+- [Frends.Community.PgpClearTextSignature](#Frends.Community.PgpClearTextSignature)
+   - [Installing](#installing)
+   - [Building](#building)
+   - [Contributing](#contributing)
+   - [Documentation](#documentation)
+      - [PgpClearTextSignature](#convertExcelFile)
+		 - [Input](#input)
+		 - [Options](#options)
+		 - [Result](#result)
+   - [License](#license)
+       
 # Frends.Community.PgpVerifyClearTextSignature
-Verifies a PGP clear text signed file.
+This repository contais FRENDS4 Community Task to verify a PGP signature at the end of text file. 
+
+## Installing
+You can install the task via Frends UI Task view or you can find the nuget package from the following nuget feed
+https://www.myget.org/F/frends/api/v3/index.json
+
+## Building
+Ensure that you have https://www.myget.org/F/frends/api/v3/index.json added to your nuget feeds
+
+Clone a copy of the repo
+
+git clone https://github.com/CommunityHiQ/Frends.Community.PgpVerifyClearTextSignature.git
+
+Restore dependencies
+
+nuget restore Frends.Community.PgpVerifyClearTextSignature
+
+Rebuild the project
+
+Run Tests with nunit3. Tests can be found under
+
+Frends.Community.PgpVerifyClearTextSignatureTests\bin\Release\Frends.Community.PgpVerifyClearTextSignature.Tests.dll
+
+Create a nuget package
+
+`nuget pack nuspec/Frends.Community.PgpVerifyClearTextSignature.nuspec`
+
+## Contributing
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+
+1. Fork the repo on GitHub
+2. Clone the project to your own machine
+3. Commit changes to your own branch
+4. Push your work back up to your fork
+5. Submit a Pull request so that we can review your changes
+
+NOTE: Be sure to merge the latest from "upstream" before making a pull request!
+
+## Documentation
+
+### PgpVerifyClearTextSignature
+
+Verifies text files signed with PGP clear text signature.
+
+#### Input
+| Property  | Type  | Description |Example|
+|-----------|-------|-------------|-------|
+| InputFile  | string | Path to file to verify. | `C:\temp\message.txt`
+| OutputFile  | string | Path to file that will be created. | `C:\temp\message_out.txt`
+| PublicKeyFile  | string | Path to public key used to verify the file. 	 | `C:\temp\publicKey.asc`
+
+#### Result
+| Property  | Type  | Description |Example|
+|-----------|-------|-------------|-------|
+| FilePath | string  | Path to verified file. Note: this is same path that was given as input parameter OutputFile. Copying that path to result will enable easy references in Frends, such as #result[PgpVerifyClearTextSignature].FilePath | `C:\temp\message_out.txt`
+| Verified | bool  | true if verification is succesfull | true
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details
